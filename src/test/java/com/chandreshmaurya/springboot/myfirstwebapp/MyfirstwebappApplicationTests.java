e1134a18d9d6163a3884d84a2894939e280d2d42
chandu-iitr <cmaurya2000@gmail.com>
2024-05-07 20:39:22 +0530

    First commit

---
+++ src/test/java/com/chandreshmaurya/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandreshmaurya.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
